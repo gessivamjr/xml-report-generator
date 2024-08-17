1caf63597123e5b0af536714a589b56414c7e7b5
Gessivam Junior <juniorgessivam@gmail.com>
2024-08-17 16:23:40 -0300

    chore: config application time zone to Brasilia

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,6 @@ module XmlReportGenerator
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.active_job.queue_adapter = :sidekiq
+    config.time_zone = 'Brasilia'
   end
 end
